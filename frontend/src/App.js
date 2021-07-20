278dd802da036d4df060010a19a819e08fe4d0d0
Ariel Elazarov <ariel.elazarov@yahoo.com>
2021-07-20 17:08:28 -0400

    Set up functionality and styles for sidebar navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -8,13 +9,18 @@ import CartScreen from './Screens/CartScreen.js';
 
 // Components
 import Navbar from './components/Navbar';
+import Backdrop from './components/Backdrop';
+import SideDrawer from './components/SideDrawer';
 
 function App() {
+
+  const [sideToggle, setSideToggle] = useState(false);
+
   return (
     <Router>
-      <Navbar  />
-      {/* Sidebar*/}
-      {/* Backdrop */}
+      <Navbar click={() => setSideToggle(true)} />
+      <SideDrawer show={sideToggle} click={() => setSideToggle(false)}/>
+      <Backdrop show={sideToggle} click={() => setSideToggle(false)}/>
       <main>
         <Switch>
           <Route exact path="/" component={HomeScreen} />
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,7 +3,7 @@ import './Navbar.css';
 import { Link } from 'react-router-dom';
 
 
-const Navbar = () => {
+const Navbar = ({ click }) => {
     return (
         <nav className="navbar">
         {/* Logo */}
@@ -31,7 +31,7 @@ const Navbar = () => {
         </ul>
 
         {/* Hamburger Menu */}
-        <div className="hamburger__menu">
+        <div className="hamburger__menu" onClick={click}>
             <div></div>
             <div></div>
             <div></div>
